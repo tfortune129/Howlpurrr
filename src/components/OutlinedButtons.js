2bceaf4ed8c29c835a5f5921e1fb064a2ccc458a
Trinisia Fortune <trinisiafortune@Trinisias-MBP.lan>
2023-03-02 00:20:40 -0500

    latest commit

--- src/components/OutlinedButtons.js
+++ src/components/OutlinedButtons.js
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 
 
 export default function OutlinedButtons() {
   return (
-    
-      <Button variant="outlined" href="/addpet">
-        Link
+
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+
+
+      <Button variant="outlined" size='large' href="/addpet">
+        <Typography variant="body1" style={{ fontFamily: "'Roboto Mono', monospace" }}>
+
+        add a new pet
+
+        </Typography>
       </Button>
+
+    </div>
    
   );
 }
