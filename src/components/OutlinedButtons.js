acd2838fb1f1d6382310b70ad87481f2bb7b98ae
Trinisia Fortune <trinisiafortune@Trinisias-MBP.lan>
2023-03-02 21:44:30 -0500

    new pet form added--needs edits

--- src/components/OutlinedButtons.js
+++ src/components/OutlinedButtons.js
@@ -11,14 +11,123 @@ export default function OutlinedButtons() {
     <div style={{ display: 'flex', justifyContent: 'center' }}>
 
 
-      <Button variant="outlined" size='large' href="/addpet">
+      {/* <Button variant="outlined" size='large' href="/addpet">
         <Typography variant="body1" style={{ fontFamily: "'Roboto Mono', monospace" }}>
 
         add a new pet
 
         </Typography>
-      </Button>
+      </Button> */}
 
+
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+            add a new pet
+            </button>
+
+
+            <div className="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLongTitle">new pet form :)</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div className="modal-body">
+                    
+                <form>
+                        <div class="form-group row">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-10">
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
+                            </div>
+                        </div>
+                        <fieldset class="form-group">
+                            <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked/>
+                                <label class="form-check-label" for="gridRadios1">
+                                    First radio
+                                </label>
+                                </div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2"/>
+                                <label class="form-check-label" for="gridRadios2">
+                                    Second radio
+                                </label>
+                                </div>
+                                <div class="form-check disabled">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled/>
+                                <label class="form-check-label" for="gridRadios3">
+                                    Third disabled radio
+                                </label>
+                                </div>
+                            </div>
+                            </div>
+                        </fieldset>
+                        <div class="form-group row">
+                            <div class="col-sm-2">Checkbox</div>
+                            <div class="col-sm-10">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck1"/>
+                                <label class="form-check-label" for="gridCheck1">
+                                Example checkbox
+                                </label>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                            </div>
+                        </div>
+                </form>
+
+
+                </div>
+                <div class="col-md-6 mb-4">
+
+                  <h6 class="mb-2 pb-1">Gender: </h6>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
+                      value="option1" checked />
+                    <label class="form-check-label" for="femaleGender">Female</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
+                      value="option2" />
+                    <label class="form-check-label" for="maleGender">Male</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
+                      value="option3" />
+                    <label class="form-check-label" for="otherGender">Other</label>
+                  </div>
+
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">close</button>
+                    <button type="button" className="btn btn-primary">save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>
+    
+    
+    
+    
     </div>
    
   );
