92940dec89e282642d220b03b71d745defcdea85
Trinisia Fortune <trinisiafortune@Trinisias-MBP.lan>
2023-03-03 00:49:05 -0500

    basic add pet done

--- src/components/OutlinedButtons.js
+++ src/components/OutlinedButtons.js
@@ -29,7 +29,7 @@ export default function OutlinedButtons() {
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
                 <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLongTitle">new pet form :)</h5>
+                    <h5 className="modal-title" id="exampleModalLongTitle"><b>new pet form :)</b></h5>
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
@@ -38,82 +38,46 @@ export default function OutlinedButtons() {
                     
                 <form>
                         <div class="form-group row">
-                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                            <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
+                            <label for="inputpetname" class="col-form-label"><b>pet name:</b></label>
+                            <div class="col-sm-12">
+                            <input type="input1" class="form-control" id="inputpetname" placeholder=""/>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                            <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
+                            <label for="inputpetage" class="col-form-label"><b>pet age <i>(in mm/dd/yyyy format):</i></b></label>
+                            <div class="col-sm-12">
+                            <input type="input2" class="form-control" id="inputpetage" placeholder=""/>
                             </div>
                         </div>
-                        <fieldset class="form-group">
-                            <div class="row">
-                            <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-                            <div class="col-sm-10">
-                                <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked/>
-                                <label class="form-check-label" for="gridRadios1">
-                                    First radio
-                                </label>
-                                </div>
-                                <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2"/>
-                                <label class="form-check-label" for="gridRadios2">
-                                    Second radio
-                                </label>
-                                </div>
-                                <div class="form-check disabled">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled/>
-                                <label class="form-check-label" for="gridRadios3">
-                                    Third disabled radio
-                                </label>
-                                </div>
-                            </div>
-                            </div>
-                        </fieldset>
-                        <div class="form-group row">
-                            <div class="col-sm-2">Checkbox</div>
-                            <div class="col-sm-10">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="gridCheck1"/>
-                                <label class="form-check-label" for="gridCheck1">
-                                Example checkbox
-                                </label>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-10">
-                            <button type="submit" class="btn btn-primary">Sign in</button>
-                            </div>
-                        </div>
+                       
+                        
+                        <label class="form-label" for="customFile">upload a picture of your cutie (optional):</label>
+                        <input type="file" class="form-control" id="customFile" />
+
                 </form>
 
 
                 </div>
-                <div class="col-md-6 mb-4">
+                <div class="col-md-8 mb-4">
 
                   <h6 class="mb-2 pb-1">Gender: </h6>
 
                   <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                      value="option1" checked />
-                    <label class="form-check-label" for="femaleGender">Female</label>
+                      value="option1" />
+                    <label class="form-check-label" for="femaleGender">female</label>
                   </div>
 
                   <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
                       value="option2" />
-                    <label class="form-check-label" for="maleGender">Male</label>
+                    <label class="form-check-label" for="maleGender">male</label>
                   </div>
 
                   <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
                       value="option3" />
-                    <label class="form-check-label" for="otherGender">Other</label>
+                    <label class="form-check-label" for="otherGender">other</label>
                   </div>
 
                 </div>
@@ -124,8 +88,6 @@ export default function OutlinedButtons() {
                 </div>
             </div>
             </div>
-    
-    
     
     
     </div>
